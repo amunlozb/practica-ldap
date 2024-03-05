06c7594ebb4e79a7097d71c1650a45f65d1a3672
amunlozb <amunlozb@g.educaand.es>
2024-03-06 00:22:53 +0100

    Initial commit

---
+++ src/main/java/org/iesalixar/daweb/practicaldap/PracticaLdapApplication.java
@@ -0,0 +1,13 @@
+package org.iesalixar.daweb.practicaldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaLdapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/iesalixar/daweb/practicaldap/PracticaLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iesalixar.daweb.practicaldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
